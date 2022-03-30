3c869dd12ba2425414bb926c2b37f34601903ac4
endo <endo0430w@gmail.com>
2022-03-31 01:04:19 +0900

    パス修正

--- src/Routers.tsx
+++ src/Routers.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import DashBoard from './components/dashboard/dashboard'
+import DashBoard from './components/View/dashboard/dashboard'
 
 const Routers: React.VFC = () => {
   return (
