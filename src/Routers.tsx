9a4043068d0a17cbbb2e0b0e3a55a7c0a5f287ae
endo <endo0430w@gmail.com>
2022-07-15 23:59:29 +0900

    Remove component

--- src/Routers.tsx
+++ src/Routers.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import DashBoard from './components/dashboard/dashboard'
 
 const Routers: React.VFC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<DashBoard />} />
+        <Route path="/" element={<></>} />
       </Routes>
     </Router>
   )
